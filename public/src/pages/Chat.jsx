23b55a51b91d1c5eea2501a24fb5af2e079dcb9d
Kartikae Khurana <55345168+kartikaekhurana@users.noreply.github.com>
2025-04-09 16:20:14 +0530

    Update Chat.jsx

--- public/src/pages/Chat.jsx
+++ public/src/pages/Chat.jsx
@@ -3,6 +3,7 @@ import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { io } from "socket.io-client";
 import styled from "styled-components";
+
 import { allUsersRoute, host } from "../utils/APIRoutes";
 import ChatContainer from "../components/ChatContainer";
 import Contacts from "../components/Contacts";
@@ -14,17 +15,21 @@ export default function Chat() {
   const [contacts, setContacts] = useState([]);
   const [currentChat, setCurrentChat] = useState(undefined);
   const [currentUser, setCurrentUser] = useState(undefined);
-  useEffect(async () => {
-    if (!localStorage.getItem(process.env.REACT_APP_LOCALHOST_KEY)) {
-      navigate("/login");
-    } else {
-      setCurrentUser(
-        await JSON.parse(
-          localStorage.getItem(process.env.REACT_APP_LOCALHOST_KEY)
-        )
-      );
-    }
-  }, []);
+
+  // Load current user from localStorage
+  useEffect(() => {
+    const fetchUser = async () => {
+      const storedUser = localStorage.getItem(process.env.REACT_APP_LOCALHOST_KEY);
+      if (!storedUser) {
+        navigate("/login");
+      } else {
+        setCurrentUser(JSON.parse(storedUser));
+      }
+    };
+    fetchUser();
+  }, [navigate]);
+
+  // Initialize socket connection and emit user ID
   useEffect(() => {
     if (currentUser) {
       socket.current = io(host);
@@ -32,32 +37,38 @@ export default function Chat() {
     }
   }, [currentUser]);
 
-  useEffect(async () => {
-    if (currentUser) {
-      if (currentUser.isAvatarImageSet) {
-        const data = await axios.get(`${allUsersRoute}/${currentUser._id}`);
-        setContacts(data.data);
-      } else {
+  // Fetch contacts only if avatar is set
+  useEffect(() => {
+    const fetchContacts = async () => {
+      if (currentUser && currentUser.isAvatarImageSet) {
+        try {
+          const { data } = await axios.get(`${allUsersRoute}/${currentUser._id}`);
+          setContacts(data);
+        } catch (err) {
+          console.error("Failed to load contacts", err);
+        }
+      } else if (currentUser) {
         navigate("/setAvatar");
       }
-    }
-  }, [currentUser]);
+    };
+    fetchContacts();
+  }, [currentUser, navigate]);
+
   const handleChatChange = (chat) => {
     setCurrentChat(chat);
   };
+
   return (
-    <>
-      <Container>
-        <div className="container">
-          <Contacts contacts={contacts} changeChat={handleChatChange} />
-          {currentChat === undefined ? (
-            <Welcome />
-          ) : (
-            <ChatContainer currentChat={currentChat} socket={socket} />
-          )}
-        </div>
-      </Container>
-    </>
+    <Container>
+      <div className="container">
+        <Contacts contacts={contacts} changeChat={handleChatChange} />
+        {currentChat === undefined ? (
+          <Welcome />
+        ) : (
+          <ChatContainer currentChat={currentChat} socket={socket} />
+        )}
+      </div>
+    </Container>
   );
 }
 
@@ -70,12 +81,14 @@ const Container = styled.div`
   gap: 1rem;
   align-items: center;
   background-color: #131324;
+
   .container {
     height: 85vh;
     width: 85vw;
     background-color: #00000076;
     display: grid;
     grid-template-columns: 25% 75%;
+
     @media screen and (min-width: 720px) and (max-width: 1080px) {
       grid-template-columns: 35% 65%;
     }
