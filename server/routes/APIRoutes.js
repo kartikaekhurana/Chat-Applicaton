ec61720d6a4a32d6423957cf5e439078931c26ba
Kartikae Khurana <55345168+kartikaekhurana@users.noreply.github.com>
2025-04-09 15:16:49 +0530

    Create APIRoutes.js

---
+++ server/routes/APIRoutes.js
@@ -0,0 +1,10 @@
+// src/utils/APIRoutes.js
+
+export const host = "https://chat-applicaton-d9uk.onrender.com";
+
+export const registerRoute = `${host}/api/auth/register`;
+export const loginRoute = `${host}/api/auth/login`;
+export const setAvatarRoute = `${host}/api/auth/setavatar`;
+export const allUsersRoute = `${host}/api/auth/allusers`;
+export const sendMessageRoute = `${host}/api/messages/addmsg`;
+export const getAllMessagesRoute = `${host}/api/messages/getmsg`;
